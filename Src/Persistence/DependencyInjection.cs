8aff2321121ce766562ef6157ad1375112546ff6
Rafał Matyjaszyk <r.matyjaszyk@smtsoftwareservices.onmicrosoft.com>
2020-03-01 21:28:07 +0000

    Merged PR 5: PC-48 Basic Project Structure
    * Directories
    * DependencyInjections
    * Startup/Program modification
    * MediatR and AutoMapper injection
    * BaseController with MediatR and Session
    * appsettings.json (empty)

---
+++ Src/Persistence/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTB.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            /*
+                services.AddScoped<IDbContext, DbContext>();
+
+                services.AddDbContext<DbContext>(options =>
+                {
+                    options.UseSqlServer(ConnectionString);
+                });
+            */
+
+            return services;
+        }
+    }
+}
---
+++ Src/Server/Controllers/ExampleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BTB.Application.Example.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTB.Server.Controllers
+{
+    public class ExampleController : BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> test()
+        {
+            return Ok(await Mediator.Send(new ExampleQuery()));
+        }
+    }
+}
