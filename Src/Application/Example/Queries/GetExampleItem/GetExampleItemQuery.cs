9d03d24f01f24ec673462ea9b45b884609e01137
Piotr Wasilewski <ptr.wasilewski@smtsoftwareservices.onmicrosoft.com>
2020-03-02 01:31:41 +0000

    Merged PR 6: Added global exception handling

---
+++ Src/Application/Example/Queries/GetExampleItem/GetExampleItemQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace BTB.Application.Example.Queries.GetExampleItem
+{
+    // Added for the purpose of testing global exception handling
+    public class GetExampleItemQuery : IRequest<int>
+    {
+        public int Id { get; set; }
+    }
+}
--- Src/Server/Program.cs
+++ Src/Server/Program.cs
@@ -1,53 +1,22 @@
-﻿using MediatR;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Threading.Tasks;
 
 namespace BTB.Server
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    /* for migration
-                    var apiContext = services.GetRequiredService<ApiDbContext>();
-                    apiContext.Database.Migrate();
-                    */
-
-                    var mediator = services.GetRequiredService<IMediator>();
-                    //await mediator.Send(new SeedSampleDataCommand(), CancellationToken.None);
-                }
-                catch (Exception e)
-                {
-                    //TODO log exception
-                }
-            }
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((hostContext, config) =>
-            {
-                var env = hostContext.HostingEnvironment;
-
-                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-                config.AddEnvironmentVariables();
-            })
-                .UseStartup<Startup>();
+                .UseConfiguration(new ConfigurationBuilder()
+                    .AddCommandLine(args)
+                    .Build())
+                .UseStartup<Startup>()
+                .Build();
     }
 }
