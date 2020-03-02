3d046ce3e006769aa7d570b9181f19995046b4ee
Mordechaj <ptr.wasilewski@outlook.com>
2020-03-02 01:09:22 +0100

    Fixed comment

--- Src/Server/Controllers/ExampleController.cs
+++ Src/Server/Controllers/ExampleController.cs
@@ -14,7 +14,7 @@ namespace BTB.Server.Controllers
         }
 
         /* Added for the purpose of testing global exception handling,
-         * How to use it: https://localhost:44358/api/example/1
+         * How to use it: https://localhost:[YOUR_PORT_NUMBER]/api/example/1
          */
         [HttpGet("{id}")]
         public async Task<ActionResult<int>> GetItem(int id)
