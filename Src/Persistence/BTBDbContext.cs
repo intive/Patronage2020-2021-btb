1cc08cf6300df11562e6af231af9bcf6028af5f6
Filip Musiał <fimusial@gmail.com>
2020-03-02 19:18:00 +0100

    Added DbContext class and an example entity.

---
+++ Src/Persistence/BTBDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BTB.Application.Common.Interfaces;
+using BTB.Domain.Example.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTB.Persistence
+{
+    public class BTBDbContext : DbContext, IBTBDbContext
+    {
+        public DbSet<ExampleEntity> ExampleEntities { get; set; }
+
+        public BTBDbContext(DbContextOptions<BTBDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        }
+    }
+}
