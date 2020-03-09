cd4b400dc3aa0d39021d96a7afb84d0c90932c08
Michał Tymejczyk <michal.tymejczyk@smtsoftwareservices.onmicrosoft.com>
2020-03-09 09:27:08 +0000

    Merged PR 27: Fix: Changed pathing to dashboard and removed unnecessary pages

--- Src/Domain/Entities/BinanceSymbolPrice.cs
+++ Src/Domain/Entities/BinanceSymbolPrice.cs
@@ -2,11 +2,9 @@
 {
     public class BinanceSymbolPrice
     {
-        // public because Dashboard.razor requires access
         public string Symbol { get; set; }
         public decimal LastPrice { get; set; }
         
-        // seem unused but it's required for GetJsonAsync<>()
         public BinanceSymbolPrice() { }
     };
 }
