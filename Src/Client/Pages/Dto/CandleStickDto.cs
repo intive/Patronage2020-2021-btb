a5fbf4086d7d12c43aff690f73af648acb6419a2
Maciej Gościmiński <m.gosciminski@smtsoftwareservices.onmicrosoft.com>
2020-03-19 13:12:38 +0000

    Merged PR 51: PC-72 Adding candlestick chart to details page
    Adding ability to switch between data table view and candlestick chart view.

---
+++ Src/Client/Pages/Dto/CandleStickDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BTB.Client.Pages.Dto
+{
+    public class CandleStickDto
+    {
+        public double OpenPrice { get; set; }
+        public double ClosePrice { get; set; }
+        public double LowestPrice { get; set; }
+        public double HighestPrice { get; set; }
+        public string OpenTime { get; set; }
+    }
+}
