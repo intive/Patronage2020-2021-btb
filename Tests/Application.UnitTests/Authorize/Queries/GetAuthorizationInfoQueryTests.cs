1e36f88845b749c48aa8971f32298131a864d7fc
Hakej <michaluszak97@gmail.com>
2020-03-31 20:58:08 +0200

    Add GetAuthorizationInfoQueryTests

---
+++ Tests/Application.UnitTests/Authorize/Queries/GetAuthorizationInfoQueryTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using BTB.Application.Authorize.Queries.GetAuthorizationInfo;
+using System.Security.Claims;
+
+namespace Application.UnitTests.Authorize.Queries
+{
+    public class GetAuthorizationInfoQueryTests : QueryTestsBase
+    {
+        [Fact]
+        public async Task Handle_ShouldReturnSameAuthorizationInfo_AsQueryRequest()
+        {
+            var expectedIsAuthenticated = true;
+            var expectedUserName = "username_test";
+
+            var sut = new GetAuthorizationInfoQueryHandler(_mapper);
+            var query = new GetAuthorizationInfoQuery();
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, expectedUserName)
+            };
+            var identity = new ClaimsIdentity(claims, "TestAuthType");
+            query.User = new ClaimsPrincipal(identity);
+
+            var result = await sut.Handle(query, CancellationToken.None);
+
+            Assert.Equal(expectedIsAuthenticated, result.IsAuthenticated);
+            Assert.Equal(expectedUserName, result.UserName);
+            Assert.Single(result.ExposedClaims);
+        }
+    }
+}
