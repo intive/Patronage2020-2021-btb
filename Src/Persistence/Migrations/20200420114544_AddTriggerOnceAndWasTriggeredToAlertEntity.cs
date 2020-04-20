13924286117da8269b4f739bfd236857c9db0774
Filip Musiał <fimusial@gmail.com>
2020-04-20 14:18:27 +0200

    Migration safety

--- Src/Persistence/Migrations/20200420114544_AddTriggerOnceAndWasTriggeredToAlertEntity.cs
+++ Src/Persistence/Migrations/20200420114544_AddTriggerOnceAndWasTriggeredToAlertEntity.cs
@@ -6,6 +6,8 @@ namespace BTB.Persistence.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("DELETE FROM Alerts;", true);
+
             migrationBuilder.AddColumn<bool>(
                 name: "TriggerOnce",
                 table: "Alerts",
@@ -21,6 +23,8 @@ namespace BTB.Persistence.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("DELETE FROM Alerts;", true);
+
             migrationBuilder.DropColumn(
                 name: "TriggerOnce",
                 table: "Alerts");
