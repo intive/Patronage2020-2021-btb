26649b23e23a6d264f46c2502776eaeb98956766
Rafal37 <rafal.matyjaszyk@tlen.pl>
2020-05-08 23:10:12 +0200

    logs console

---
+++ Src/Domain/ValueObjects/LogOptionsVO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTB.Domain.ValueObjects
+{
+    public class LogOptionsVO
+    {
+        public bool Information { get; set; }
+        public bool Warning { get; set; }
+        public bool Error { get; set; }
+        public bool Critical { get; set; }
+    }
+}
